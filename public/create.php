91551ad70d636a94191b8ae0c3cd631188be9ea2
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-11-30 15:38:29 -0800

    fleshing out more of the create form

--- public/create.php
+++ public/create.php
@@ -1,18 +1,27 @@
 <?php include "templates/header.php" ?>
-<form>
+<form method="post">
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="first_name">First Name</label>
+    <input type="text" class="form-control" id="first_name" name="first_name" aria-describedby="text" placeholder="Enter first name">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="last_name">Last Name</label>
+    <input type="text" class="form-control" id="last_name" name="last_name" aria-describedby="text" placeholder="Enter last name">
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" name="email" aria-describedby="text" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="age">Age</label>
+    <input type="text" class="form-control" id="age" name="age" aria-describedby="text" placeholder="Enter age">
+  </div>
+  <div class="form-group">
+    <label for="location">Location</label>
+    <input type="text" class="form-control" id="location" name="location" aria-describedby="text" placeholder="Enter location">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+<a href="index.php">< Back to Home</a>
 <?php include "templates/footer.php" ?>
