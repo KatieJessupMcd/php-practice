7c6fcd489d0fe2d0c11afaba083f7fefac67f3f8
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-11-30 14:11:39 -0800

    beginning to flesh out file structure, got virtual host set up

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Simple Database App</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <h1>Simple Database App</h1>
+
+  <ul>
+    <li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+    <li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+  </ul>
+
+</body>
+</html>
