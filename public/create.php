21d0598c5bc8d106babdd2b09fb06e7bbb4e33fc
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-12-01 13:26:15 -0800

    added some more formatting to the create form

--- public/create.php
+++ public/create.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 require "../config.php";
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
   try {
     $connection = new PDO($dsn, $username, $password, $options);
-    
+
     $new_user = array(
       "firstname" => $_POST['firstname'],
-      "lastname" => $_POST['lastname'], 
+      "lastname" => $_POST['lastname'],
       "email" => $_POST['email'],
       "age" => $_POST['age'],
       "location" => $_POST['location']
@@ -19,41 +19,50 @@ if(isset($_POST['submit'])){
       "users",
       implode(", ", array_keys($new_user)),
       ":" . implode(", :", array_keys($new_user))
-  );
+    );
 
     $statement = $connection->prepare($sql);
     $statement->execute($new_user);
-  } catch(PDOException $error){
+  } catch (PDOException $error) {
     echo $sql . "<br>" . $error->getMessage();
   }
-} 
+}
 ?>
 
 <?php require "templates/header.php"; ?>
 
-<form method="post">
-  <div class="form-group">
-    <label for="firstname">First Name</label>
-    <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="text" placeholder="Enter first name">
+<div class="container">
+  <div class="row">
+    <div class="col-3">
+    </div>
+    <div class="col-6">
+      <form method="post">
+        <div class="form-group">
+          <label for="firstname">First Name</label>
+          <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="text" placeholder="Enter first name">
+        </div>
+        <div class="form-group">
+          <label for="lastname">Last Name</label>
+          <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="text" placeholder="Enter last name">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" name="email" aria-describedby="text" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="age">Age</label>
+          <input type="text" class="form-control" id="age" name="age" aria-describedby="text" placeholder="Enter age">
+        </div>
+        <div class="form-group">
+          <label for="location">Location</label>
+          <input type="text" class="form-control" id="location" name="location" aria-describedby="text" placeholder="Enter location">
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <div class="col-3">
+      </div>
+    </div>
   </div>
-  <div class="form-group">
-    <label for="lastname">Last Name</label>
-    <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="text" placeholder="Enter last name">
-  </div>
-  <div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" name="email" aria-describedby="text" placeholder="Enter email">
-  </div>
-  <div class="form-group">
-    <label for="age">Age</label>
-    <input type="text" class="form-control" id="age" name="age" aria-describedby="text" placeholder="Enter age">
-  </div>
-  <div class="form-group">
-    <label for="location">Location</label>
-    <input type="text" class="form-control" id="location" name="location" aria-describedby="text" placeholder="Enter location">
-  </div>
-  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-</form>
 
-<a href="index.php">< Back to Home</a>
-<?php require "templates/footer.php"; ?>
+  <a href="index.php">
+    < Back to Home</a> <?php require "templates/footer.php"; ?>
